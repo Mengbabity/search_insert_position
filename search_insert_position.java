c3aecbd4c8a3ddb0e06f70c29ee7c68d999b1825
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2018-01-25 18:06:07 +0800

    Create search_insert_position.java

---
+++ search_insert_position.java
@@ -0,0 +1,10 @@
+package search_insert_position;
+
+public class search_insert_position {
+public static void main(String[] args)
+{
+	solution s=new solution();
+	int[] array={1,3,5,7};
+	s.searchInsert(array, 6);
+}
+}
